463fff42d1ed68e561ecaa6092d04e57a3042c93
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2023-10-20 17:13:53 -0300

    se añaden los estilos

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export const GifExpertApp = () => {
   return (
     <>
+    {/*Titulo de la app*/}
     <h1>GifExpertApp</h1>
+    <hr />
+    {/*Input para buscar gifs*/}
+        {/*Listado de gif*/}
     </>
   )
 }
